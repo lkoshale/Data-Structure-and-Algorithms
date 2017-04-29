2a757f9b440e1682a37b3db3f4bccb59e1e4bd51
Lokesh Koshale <lkoshale@gmail.com>
2017-04-29 15:12:45 +0530

    Update stack(List).c

--- Stack And Queue/stack(List).c
+++ Stack And Queue/stack(List).c
@@ -3,6 +3,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Stack{
+	LList* top;
+}stack;
+
+//function declaration
+// create a new stack
+stack* stack_new();
+
+// push an element on the stack
+void stack_push(stack*, int);
+
+// pop the top element from the stack
+int stack_pop(stack*);
+
+// Check if stack is empty
+bool stack_is_empty(stack*);
+
+// find the size of the stack
+int stack_size(stack*);
+
+// print stack element
+void stack_print(stack*);
+
+
+
+
 // push an element on the stack
 stack* stack_new(){
 	stack* st=(stack*) malloc(sizeof(stack));
